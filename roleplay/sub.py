8fb33661aead1a3d6de1ea34a785cacb9e87d1ff
tigervalley1988 <81676636+tigervalley1988@users.noreply.github.com>
2021-04-01 14:08:09 +0900

    Update sub.py

--- roleplay/sub.py
+++ roleplay/sub.py
@@ -527,3 +527,4 @@ def outputFunc(fleetAll,startYear,elapsedYear,lastYear,tOpSch,decisionListName):
         np.savetxt(decisionListName+'_gTilde.csv',gTildePlot)
         np.savetxt(decisionListName+'_g.csv',gPlot)
         np.savetxt(decisionListName+'_dcostShippingTilde.csv',dcostShippingTildePlot)
+        np.savetxt(decisionListName+'_rocc.csv',roccPlot)
