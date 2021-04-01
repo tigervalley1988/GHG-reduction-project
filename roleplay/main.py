64b2468b6be24e6d27484bb0cb8618de48c42cf6
tigervalley1988 <81676636+tigervalley1988@users.noreply.github.com>
2021-04-01 10:46:04 +0900

    Update main.py

--- roleplay/main.py
+++ roleplay/main.py
@@ -41,6 +41,7 @@ def roleplayRun(decisionListName):
     fleets['year'] = np.zeros(lastYear-startYear+1)
     fleets.setdefault('output',{})
     fleets['output']['g'] = np.zeros(lastYear-startYear+1)
+    fleets['output']['cta'] = np.zeros(lastYear-startYear+1)
     fleets['output']['gTilde'] = np.zeros(lastYear-startYear+1)
     fleets['output']['dcostShippingTilde'] = np.zeros(lastYear-startYear+1)
     initialFleets = rs.initialFleetFunc(parameterFile6)
